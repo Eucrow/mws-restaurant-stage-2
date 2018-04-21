2eb1d1f3d69e6476fb5571122337b84b834bbc65
Marco A. Amez <marco.a.amez@gmail.com>
2018-04-21 18:59:39 +0200

    Add sw, offline web

--- sw.js
+++ sw.js
@@ -4,14 +4,44 @@ self.addEventListener('install', function(event){
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache){
       return cache.addAll([
-        '/', // Important include the root.. Why?? I dont know
-        '/index.html',
-        '/restaurant.html',
-        '/js/main.js',
-        '/js/dbhelper.js',
-        '/js/restaurant_info.js',
-        '/css/styles.css',
-        '/data/restaurants.json'
+        // '/', // Important include the root.. Why?? I dont know
+        'index.html',
+        'restaurant.html',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'data/restaurants.json',
+        'img/1_350.jpg',
+        'img/1_700.jpg',
+        'img/1_800.jpg',
+        'img/2_350.jpg',
+        'img/2_700.jpg',
+        'img/2_800.jpg',
+        'img/3_350.jpg',
+        'img/3_700.jpg',
+        'img/3_800.jpg',
+        'img/4_350.jpg',
+        'img/4_700.jpg',
+        'img/4_800.jpg',
+        'img/5_350.jpg',
+        'img/5_700.jpg',
+        'img/5_800.jpg',
+        'img/6_350.jpg',
+        'img/6_700.jpg',
+        'img/6_800.jpg',
+        'img/7_350.jpg',
+        'img/7_700.jpg',
+        'img/7_800.jpg',
+        'img/8_350.jpg',
+        'img/8_700.jpg',
+        'img/8_800.jpg',
+        'img/9_350.jpg',
+        'img/9_700.jpg',
+        'img/9_800.jpg',
+        'img/10_350.jpg',
+        'img/10_700.jpg',
+        'img/10_800.jpg'
       ]);
     })
   );
@@ -33,15 +63,18 @@ self.addEventListener('activate', function(event){
 });
 
 self.addEventListener('fetch', function(event){
+
   event.respondWith(
-    caches.match(event.request).then(function(response){
+  
+    caches.match(event.request, {ignoreSearch:true}).then(function(response){
       if (response){
         console.log('Found ', event.request.url, ' in cache');
         return response;
       }
       // console.log('NOT FOUND ', event.request.url, ' in cache');
       return fetch(event.request);
-    }));
+    })
+  );
   
 });
 
