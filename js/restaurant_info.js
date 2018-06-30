eae566a9e0d242432ad87a169ad1835bcd4106ad
Marco A. Amez <marco.a.amez@gmail.com>
2018-06-30 14:16:50 +0200

    Remove sw

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,13 +4,13 @@ var map;
 /**
  * Register the service worker
  */
-if (navigator.serviceWorker){
-  navigator.serviceWorker.register('sw.js').then(function(){
-    console.log('Registration worked!');
-    }).catch(function(){
-    console.log('Registration failed!');
-  });
-}
+// if (navigator.serviceWorker){
+//   navigator.serviceWorker.register('sw.js').then(function(){
+//     console.log('Registration worked!');
+//     }).catch(function(){
+//     console.log('Registration failed!');
+//   });
+// }
 
 /**
  * Initialize Google map, called from HTML.
